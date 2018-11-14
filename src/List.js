aecb10dd21193ecf373f29001b078a3dd540b4f3
hushicai <bluthcy@gmail.com>
2018-11-14 14:45:02 +0800

    fix: list callback memoization

--- src/List.js
+++ src/List.js
@@ -7,12 +7,27 @@ const data = new Array(100).fill(0).map((v, i) => {
 });
 
 class List extends Component {
+   // Each instance of SomeComponent has a cache of click handlers
+  // that are unique to it.
+  clickHandlers = {};
+
+  // Generate and/or return a click handler,
+  // given a unique identifier.
+  getClickHandler(key) {
+
+    // If no click handler exists for this unique identifier, create one.
+    if (!Object.prototype.hasOwnProperty.call(this.clickHandlers, key)) {
+      this.clickHandlers[key] = () => alert(key);
+    }
+    return this.clickHandlers[key];
+  }
+
   render() {
     return (
       <ul>
         {data.map(listItem =>
           <li key={listItem.text}>
-            <Button text={listItem.text} onClick={() => alert(listItem.text)} />
+            <Button text={listItem.text} onClick={this.getClickHandler(listItem.text)} />
           </li>
         )}
       </ul>
