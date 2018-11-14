c65bea1f0e1711cc8306df80341204b4e045268d
hushicai <bluthcy@gmail.com>
2018-11-14 15:07:03 +0800

    anti-pattern: PureComponent复杂数据类型

--- src/App.js
+++ src/App.js
@@ -1,90 +1,37 @@
 import React, { Component, PureComponent } from 'react';
 
-class A extends Component {
-  state = {
-    value: 1
+
+class ListOfWords extends PureComponent {
+  render() {
+    return <div>{this.props.words.join(',')}</div>;
+  }
+}
+
+class WordAdder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: ['marklar']
+    };
+  }
+
+  handleClick = () => {
+    // This section is bad style and causes a bug
+    const words = this.state.words;
+    words.push('marklar');
+    this.setState({words: words});
   };
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        value: 2
-      });
-    }, 5000);
-  }
-
-  b2Children = (
-    <div className="c4">
-      <h3>C4</h3>
-    </div>
-  )
-
-  onCallback = () => {};
 
   render() {
     return (
-      <div className="App">
-        <h1>A</h1>
-        <B1 value={this.state.value} />
-        <B2 onCallback={this.onCallback} />
-        <B2 onCallback={this.onCallback}>{this.b2Children}</B2>
+      <div>
+        <button onClick={this.handleClick}>
+          add word
+        </button>
+        <ListOfWords words={this.state.words} />
       </div>
     );
   }
 }
 
-class B1 extends Component {
-  render() {
-    return (
-      <div className="b1">
-        <h2>B1</h2>
-        <C1 />
-        <C2 value={this.props.value} />
-      </div>
-    );
-  }
-}
-
-class B2 extends PureComponent {
-  render() {
-    return (
-      <div className="b2">
-        <h2>B2</h2>
-        <C3 />
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-class C1 extends PureComponent {
-  render() {
-    return (
-      <div className="c1">
-        <h3>C1</h3>
-      </div>
-    );
-  }
-}
-
-class C2 extends Component {
-  render() {
-    return (
-      <div className="c2">
-        <h3>C2</h3>
-        <div>{this.props.value}</div>
-      </div>
-    );
-  }
-}
-
-class C3 extends PureComponent {
-  render() {
-    return (
-      <div className="c3">
-        <h3>C3</h3>
-      </div>
-    );
-  }
-}
-
-export default A;
+export default WordAdder;
